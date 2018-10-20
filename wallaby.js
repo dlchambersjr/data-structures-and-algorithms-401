43dd45eeddfbb9670068432cd6b59cec422232cf
David Chambers <dlchambersjr@gmail.com>
2018-10-19 20:21:05 -0700

    Save Point before Wallaby

---
+++ wallaby.js
@@ -0,0 +1,28 @@
+module.exports = function () {
+
+  return {
+
+    files: ['linked-list/**/*.js', '!linked-list/**/*.test.js'],
+  
+
+    tests: ['linked-list/**/*.test.js'],
+
+    env: {
+
+      type: 'node',
+
+      runner: 'node',
+
+      params: {
+
+        runner: '--harmony',
+
+      },
+
+    },
+
+    testFramework: 'jest',
+
+  };
+
+};
