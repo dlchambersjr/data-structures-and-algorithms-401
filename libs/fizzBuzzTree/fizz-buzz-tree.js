f30727fb8dbb9866e62d41191c7e1057b3abbe01
David Chambers <dlchambersjr@gmail.com>
2018-11-05 13:44:46 -0800

    Save Point

---
+++ libs/fizzBuzzTree/fizz-buzz-tree.js
@@ -0,0 +1,40 @@
+'use strict';
+
+class Node {
+  constructor(value = null, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+
+} //end of Node class defintition
+
+class BinaryTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+
+  fizzBuzzTree(tree = this.root) {
+
+    const traversePre = (node) => {
+      if (node) {
+        if (node.value % 15 === 0) node.value = 'fizzBuzz';
+        if (node.value % 5 === 0) node.value = 'buzz';
+        if (node.value % 3 === 0) node.value = 'fizz';
+        traversePre(node.left);
+        traversePre(node.right);
+      }
+      return tree;
+    };
+
+    if (!this.root) { return 'EMPTY TREE'; }
+    else { return traversePre(tree.root); }
+
+  }//end of fizzBuzzTree
+
+}//end of Binary tree class definition
+
+module.exports = {
+  Node,
+  BinaryTree,
+};
