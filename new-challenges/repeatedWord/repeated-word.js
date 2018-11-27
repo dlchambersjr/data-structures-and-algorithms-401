5fa8b5c2952a6a5b721adbb08b47976d283fbd1c
David Chambers <dlchambersjr@gmail.com>
2018-11-27 00:03:00 -0800

    tests complete, readme updated

--- new-challenges/repeatedWord/repeated-word.js
+++ new-challenges/repeatedWord/repeated-word.js
@@ -1,17 +1,128 @@
 'use strict';
 
-const LinkedList = require('../linked-list/linked-list.js');
+class HashMap {
+
+  constructor(size) {
+    this.size = size;
+    this.map = new Array(size);
+  }
+
+  hash(key) {
+    let total = 0;
+    let primeNum = 31;
+
+    for (let i = 0; i < key.length; i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0);
+      total = (total * primeNum + value) % this.size;
+    }
+
+    return total;
+  }
+
+  add(key, value) {
+    if (!key || !value) { return 'Key/Value Required'; }
+    let index = this.hash(key);
+
+    if (!this.map[index]) { this.map[index] = []; }
+
+    this.map[index].push({ [key]: value });
+
+  }
 
 
-const findDuplicateWord = (words = null) => {
+  find(key) {
+    if (!key) { return 'Key Required'; }
 
-  if (!words) { return 'A string is required'; }
+    let index = this.hash(key);
+    let found;
 
-  const wordTracker = new LinkedList();
-  let word = '';
-  let found = false;
+    if (this.map[index]) {
+      found = this.map[index].find(contents => Object.keys(contents) == key);
+    }
 
+    if (found) { return found[key]; }
 
+    return 'Key not Found';
+
+  }
+
+  contains(key) {
+    if (!key) { return 'Key Required'; }
+
+    let index = this.hash(key);
+    let found;
+
+    if (this.map[index]) {
+      found = this.map[index].find(contents => Object.keys(contents) == key);
+    }
+
+    if (found) { return true; }
+
+    return false;
+
+  }
+
+  getHash(key) {
+    if (!key) { return 'Key Required'; }
+    let index = this.hash(key);
+    return index;
+  }
+
+  serialize() {
+    let output = [];
+
+    for (let i = 0; i < this.map.length; i++) {
+      if (this.map[i]) {
+        output = output.concat(this.map[i]);
+      }
+    }
+
+    if (output.length) {
+      return JSON.stringify(output);
+    } else { return 'Empty Hash Table'; }
+
+  }
+
+  deserialize(input) {
+
+    if (!input) { return 'Key Required'; }
+
+    try {
+      const convertInput = JSON.parse(input);
+
+      for (let i = 0; i < convertInput.length; i++) {
+        let key = Object.keys(convertInput[i]);
+        this.add(key[0], convertInput[i][key]);
+      }
+
+      return this;
+
+    } catch (error) { return 'Not Valid JOSN'; }
+
+  }
+
+}
+
+const repeatedWord = function (str) {
+  if (typeof str !== 'string') return 'Not a string';
+  let strMap = new HashMap(str.length);
+
+  // FIXME: Permission from Joh nto use .split() with regex.  Discussion to take place in classroom.
+
+  let words = str.split(/\b/g);
+
+  for (let i = 0; i < words.length; i++) {
+    // console.log(words[i]);
+    if (strMap.contains(words[i])) {
+      return words[i];
+    } else if (words[i] !== ' ') {
+      strMap.add(words[i].toLowerCase(), words[i]);
+      // console.log(strMap.map);
+    }
+  }
+
+  return 'No duplicate words';
 };
 
-export default findDuplicateWord;
+module.exports = repeatedWord;
