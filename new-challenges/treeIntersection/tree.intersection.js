775f9fae8c612896e8bc895b871340fe12d39e0c
David Chambers <dlchambersjr@gmail.com>
2018-11-27 14:49:39 -0800

    testing started

---
+++ new-challenges/treeIntersection/tree.intersection.js
@@ -0,0 +1,41 @@
+'use strict';
+
+class BinaryTree {
+  constructor(root = null) {
+    this.root = root;
+  }
+}
+
+const HashMap = require('../hashtable/hashtable.js');
+
+const treeIntersection = (treeA, treeB) => {
+
+  const findIntersection = new HashMap(50);
+  const outputArray = [];
+
+  const inOrderFind = (node) => {
+
+    if (node) {
+      inOrderFind(node.left, outputArray);
+
+      if (findIntersection.contains(node.value)) {
+        outputArray.push(node.value);
+      } else findIntersection.add(node.value, node.value);
+
+      inOrderFind(node.right, outputArray);
+    }
+
+  };
+
+  inOrderFind(treeA.root);
+  inOrderFind(treeB.root);
+
+  return outputArray;
+
+  // if (!this.root) { return 'EMPTY TREE'; }
+  // else { return traverseIn(node, []); }
+
+};//end of inOrder
+
+
+module.exports = treeIntersection, BinaryTree;
