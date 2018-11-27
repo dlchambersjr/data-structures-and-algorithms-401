948a700dfb240bef0bf81c06cf58650212bdba90
David Chambers <dlchambersjr@gmail.com>
2018-11-27 00:01:45 -0800

    tests complete, readme updated

--- tests/repeated-word.test.js
+++ tests/repeated-word.test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const findDuplicateWord = require('../repeatedWord/repeated-word.js');
+const repeatedWord = require('../new-challenges/repeatedWord/repeated-word.js');
 
-const noWords = '';
+const noDupes = 'There are no duplicate wwords in this sentence.';
 
 const stringOfWords = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...';
 
@@ -12,19 +12,38 @@ describe('Check for the first duplicate word', () => {
 
   it('should return an error message if a string is not passed', () => {
 
-    const actual = findDuplicateWord();
-    const expected = 'A string is requiredXXX';
+    const actual = repeatedWord();
+    const expected = 'Not a string';
 
     expect(actual).toBe(expected);
 
 
   });
 
-  it('should return no words present if the the string is empty', () => { });
+  it('should return the first duplicated word in a long string', () => {
 
-  it('should return the first duplicated word', () => { });
+    const actual = repeatedWord(stringOfWords);
+    const expected = 'it';
 
-  it('should return the first duplicated word regardless of case', () => { });
+    expect(actual).toBe(expected);
+
+  });
+
+  it('should return the first duplicated word in a mixed case string', () => {
+
+    const actual = repeatedWord(mixedCase);
+    const expected = 'the';
+
+    expect(actual).toBe(expected);
+
+  });
+
+  it('should return no duplicate words if the the string has no duplicates', () => {
+    const actual = repeatedWord(noDupes);
+    const expected = 'No duplicate words';
+
+    expect(actual).toBe(expected);
+  });
 
 });
 
